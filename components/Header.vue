f6bcb78ec189b387866ceb137b7e875558aa10b1
Vinicius Mota <motvinicius@gmail.com>
2021-04-08 08:58:08 -0300

    First commit

---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+		<nuxt-link to="/">
+    	<div class="title">My blog</div>
+		</nuxt-link>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+	padding: 15px 30px;
+	background-color: #FFF;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+}
+header .title {
+	color: #212121;
+	font-size: 24px;
+	font-weight: 900;
+	text-transform: uppercase;
+}
+nav {
+	display: flex;
+	align-items: center;
+	margin: 0 -15px;
+}
+nav a {
+	display: block;
+	margin: 0 15px;
+	color: #9f25cf;
+}
+</style>
